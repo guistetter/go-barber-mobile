eb46cbdb8c09daa1992d22c9698b3913ad5957f0
guistetter <guilherme_stetter@hotmail.com>
2020-10-19 14:56:51 -0300

    apply Button

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Text } from 'react-native';
 import Background from '../../components/Background';
 import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 export default function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
       <Input icon="call" placeholder="Digite seu nome" />
+
+      <Button>Entrar</Button>
     </Background>
   );
 }
