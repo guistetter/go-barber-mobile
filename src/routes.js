9b14e42e8539d4156ec7d8a6a72aab0f48e287e4
guistetter <guilherme_stetter@hotmail.com>
2020-10-19 15:56:11 -0300

    eslint fix

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
-import {createAppContainer, createSwitchNavigator } from 'react-navigation'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 export default createAppContainer(
   createSwitchNavigator({
     SignIn,
-    SignUp
+    SignUp,
   })
-)
+);
