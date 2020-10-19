81e3f15e632739746eec6797f535998f399041ec
guistetter <guilherme_stetter@hotmail.com>
2020-10-19 15:47:10 -0300

    crate form login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+import logo from '../../assets/logo.png';
 import Background from '../../components/Background';
-import Input from '../../components/Input';
-import Button from '../../components/Button';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
-      <Input icon="call" placeholder="Digite seu nome" />
-
-      <Button>Entrar</Button>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu email"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
