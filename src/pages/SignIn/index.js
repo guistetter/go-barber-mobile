2c04f7f664ca0c343d35da52cd7979c0d3d246f5
guistetter <guilherme_stetter@hotmail.com>
2020-10-19 14:46:08 -0300

    apply icon

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Text } from 'react-native';
 import Background from '../../components/Background';
+import Input from '../../components/Input';
 
 export default function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+      <Input icon="call" placeholder="Digite seu nome" />
     </Background>
   );
 }
