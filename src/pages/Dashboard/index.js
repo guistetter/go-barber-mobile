04dd9397a143bfbdddf9b92bc2a88c3334f1ec27
guistetter <guilherme_stetter@hotmail.com>
2020-10-20 15:31:16 -0300

    apply background

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import Background from '../../components/Background';
 
 export default function Dashboard() {
-  return <View />;
+  return <Background />;
 }
 Dashboard.navigationOptions = {
   tabBarLabel: 'Agendamentos',
