d4f8b611030033a006e8c50f168f056ea89155d7
guistetter <guilherme_stetter@hotmail.com>
2020-10-20 14:10:08 -0300

    create loading

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import logo from '../../assets/logo.png';
 import Background from '../../components/Background';
 import { signUpRequest } from '../../store/modules/auth/actions';
@@ -21,6 +21,7 @@ export default function SignIn({ navigation }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const loading = useSelector((state) => state.auth.loading);
 
   function handleSubmit() {
     dispatach(signUpRequest(name, email, password));
@@ -63,7 +64,9 @@ export default function SignIn({ navigation }) {
             value={password}
             onChangeText={setPassword}
           />
-          <SubmitButton onPress={handleSubmit}>Criar Conta</SubmitButton>
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Criar Conta
+          </SubmitButton>
         </Form>
         <SignLink onPress={() => navigation.navigate('SignIn')}>
           <SignLinkText>Já tenho conta</SignLinkText>
