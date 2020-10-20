a7ecf125f491713c4a8c790e006c2b0781b87713
guistetter <guilherme_stetter@hotmail.com>
2020-10-20 17:22:54 -0300

    create list of appointments

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Left, Avatar, Info, Name, Time } from './styles';
+
+export default function Appointment() {
+  return (
+    <Container>
+      <Left>
+        <Avatar
+          source={{
+            uri:
+              'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=50',
+          }}
+        />
+        <Info>
+          <Name>Gui </Name>
+          <Time>em 3 horas</Time>
+        </Info>
+      </Left>
+      <TouchableOpacity onPress={() => {}}>
+        <Icon name="event-busy" size={20} color="#f64c75" />
+      </TouchableOpacity>
+    </Container>
+  );
+}
