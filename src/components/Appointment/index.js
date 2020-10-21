5e23dad099a78c1dc52c5ce7c59cc2cf6928f0d8
guistetter <guilherme_stetter@hotmail.com>
2020-10-21 11:42:24 -0300

    list appointment from api

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { parseISO, formatRelative } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Left, Avatar, Info, Name, Time } from './styles';
 
-export default function Appointment() {
+export default function Appointment({ data }) {
+  const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(data.date), new Date(), {
+      locale: pt,
+      addSuffix: true,
+    });
+  }, [data.date]);
   return (
-    <Container>
+    <Container past={data.past}>
       <Left>
         <Avatar
           source={{
-            uri:
-              'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=50',
+            uri: data.provider.avatar
+              ? data.provider.avatar.url
+              : `https://api.adorable.io/avatar/50/${data.provider.name}.png`,
           }}
         />
         <Info>
-          <Name>Gui </Name>
-          <Time>em 3 horas</Time>
+          <Name>{data.provider.name} </Name>
+          <Time>{dateParsed}</Time>
         </Info>
       </Left>
       <TouchableOpacity onPress={() => {}}>
