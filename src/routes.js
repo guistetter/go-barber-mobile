48f06af3cf98a06b514d764bc7526c5dc8e1fa45
guistetter <guilherme_stetter@hotmail.com>
2020-10-21 16:59:27 -0300

    add configs in navigation options

--- src/routes.js
+++ src/routes.js
@@ -24,12 +24,24 @@ export default (signedIn = false) =>
           {
             Dashboard,
             New: {
-              screen: createStackNavigator({
-                SelectProvider,
-                SelectDateTime,
-                Confirm,
-              }),
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm,
+                },
+                {
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: '#fff',
+                    headerLeftContainerStyle: {
+                      marginLeft: 20,
+                    },
+                  },
+                }
+              ),
               navigationOptions: {
+                tabBarVisible: false,
                 tabBarLabel: 'Agendar',
                 tabBarIcon: (
                   <Icon
