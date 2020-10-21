9c50e057e7d2cd83997e12a70dde196d3d949741
guistetter <guilherme_stetter@hotmail.com>
2020-10-21 13:46:19 -0300

    add cancel appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,7 +6,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Left, Avatar, Info, Name, Time } from './styles';
 
-export default function Appointment({ data }) {
+export default function Appointment({ data, onCancel }) {
   const dateParsed = useMemo(() => {
     return formatRelative(parseISO(data.date), new Date(), {
       locale: pt,
@@ -28,9 +28,12 @@ export default function Appointment({ data }) {
           <Time>{dateParsed}</Time>
         </Info>
       </Left>
-      <TouchableOpacity onPress={() => {}}>
-        <Icon name="event-busy" size={20} color="#f64c75" />
-      </TouchableOpacity>
+
+      {data.cancelable && !data.canceled_at && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="event-busy" size={20} color="#f64c75" />
+        </TouchableOpacity>
+      )}
     </Container>
   );
 }
