9a83ef59aff16c7bc5a3a9d268b2e73746617212
guistetter <guilherme_stetter@hotmail.com>
2020-10-22 11:30:08 -0300

    create select hour

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,14 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '../../../components/Background';
 import DateInput from '../../../components/DateInput';
+import api from '../../../services/api';
+import { HourList, Hour, Title } from './styles';
 
-export default function SelectDateTime() {
+export default function SelectDateTime({ navigation }) {
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  const provider = navigation.getParam('provider');
+
+  useEffect(() => {
+    async function loadAvailable() {
+      const response = await api.get(`providers/${provider.id}/available`, {
+        params: { date: date.getTime() },
+      });
+      setHours(response.data);
+    }
+    loadAvailable();
+  }, [date, provider.id]);
+
+  function handleSelectHour(time) {
+    navigation.navigate('Confirm', {
+      provider,
+      time,
+    });
+  }
+
   return (
     <Background>
       <DateInput date={date} onChange={setDate} />
+
+      <HourList
+        data={hours}
+        keyExtractor={(item) => item.time}
+        renderItem={({ item }) => (
+          <Hour
+            onPress={() => handleSelectHour(item.value)}
+            enabled={item.available}
+          >
+            <Title>{item.time}</Title>
+          </Hour>
+        )}
+      />
     </Background>
   );
 }
